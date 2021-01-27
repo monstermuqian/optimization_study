b788b18478eff7c9bf9d053d4b32b920963aec25
chen4 <Chen4@fzi.de>
2021-01-27 23:21:41 +0100

    The function of KKT-Matrix is completed. Next is to consider about how to implement the management of working set.

---
+++ python/EQP_Solution.py
@@ -0,0 +1,31 @@
+# A has a dimension of (m,n) m row, n column.
+# Each row contain parameters of constraints. There are totally m constraints.
+# b has a dimension of (m,1) m row, 1 column
+# G has a dimension of (n,n) n row, n column
+# d has a dimension of (n,1) n row, 1 column
+# theta has a dimension of (n,1) n row, 1 column
+
+import numpy as np
+
+def EQP_Solution (A, b, G, d, theta):
+    g = np.dot(G, theta) + d
+    h = np.dot(A, theta) - b
+
+    # concatenate the matrix into a KKT-Matrix and [g;h] vector
+    [row, col] = np.shape(A)
+    K_temp1 = np.concatenate((G, np.transpose(A)), axis=1)
+    zeros_matrix = np.zeros([row, row])
+    K_temp2 = np.concatenate((A, zeros_matrix), axis=1)
+
+    K = np.concatenate((K_temp1, K_temp2), axis=0)
+    g_h = np.concatenate((g, h), axis=0)
+
+    # calculation the step length and lambda number
+    K_inv = np.linalg.inv(K)
+    result = np.dot(K_inv, g_h)
+
+    # from result separate the step length and lambda value
+    step_length = -result[0:col, 0].reshape(col, 1)
+    lambda_star = result[col:, 0].reshape(row, 1)
+
+    return step_length, lambda_star
--- python/SQP_solution.py
+++ python/SQP_solution.py
@@ -3,22 +3,75 @@
 # Purpose: Realisation of algorithms for sequential quadratic programming for python version
 # I hope i can success.
 
+# 27.01.2021
+# After cross testing with MATLAB is proved that the algorithm EQP can work fine.
+
 
 import numpy as np
-import EQP_Solution as eqp
+from EQP_Solution import EQP_Solution as eqp
 import compare_blocking as com_bl
 
 
-G = np.array([[2, 0], [0, 2]])
-# 在python中定义 2 x 1 向量也是要打上方括号的
-d = np.array([[-2], [-5]])
+def main():
 
-#print(np.shape(d))
+    G = np.array([[2, 0],
+                  [0, 2]])
+    # 在python中定义 2 x 1 向量也是要打上方括号的
+    d = np.array([[-2],
+                  [-5]])
+    theta = np.array([[0],
+                        [1]])
+    #print(np.shape(d))
 
-A_equation = np.empty((2,2))
-b_equation = np.array([[], []])
-A_inequation = np.array([[1, -2], [-1, -2], [-1, 2], [1, 0], [0, 1]])
-b_inequation = np.array([[-2], [-6], [-2], [0], [0]])
+    A_equation = np.array([[], []])
+    b_equation = np.array([[], []])
+    A_inequation = np.array([[ 1, -2],
+                             [-1, -2],
+                             [-1, 2],
+                             [ 1, 0],
+                             [ 0, 1]])
+    b_inequation = np.array([[-2],
+                             [-6],
+                             [-2],
+                             [ 0],
+                             [ 0]])
+
+    A = np.vstack((A_inequation[3, :], A_inequation[0, :]))
+    b = np.vstack((b_inequation[3, :], b_inequation[0, :]))
 
 
-print(np.shape(A_equation))
+    #[step_length, lambda_star] = eqp(A, b, G, d, theta)
+    #print(step_length)
+    #print(lambda_star)
+
+    #inequation = {1: b_inequation[0, :].reshape(1, 1)}
+
+    b_working = np.empty((1, 1))
+    print(b_working)
+    b_working = np.vstack((b, b_working))
+    print(b_working)
+    b_working = np.delete(b_working, 1, axis=0)
+    print(b_working)
+
+    dict = {}
+    list = []
+
+
+
+    while True:
+        [step_length, lambda_star] = eqp(A, b, G, d, theta)
+
+        if step_length.all() == 0:
+            if lambda_star.all() > 0:
+                return 0
+            else:
+                return 0
+        else:
+            return 0
+
+
+
+
+if __name__ == '__main__':
+    main()
+
